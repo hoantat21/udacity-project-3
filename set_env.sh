f037853e012ec92b4cff5bf331d4bd4c2a809bcc
hoantat21 <36986868+hoantat21@users.noreply.github.com>
2023-05-30 22:13:52 +0700

    init file

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=123123123
+export POSTGRES_HOST=cdr.cnsbuts3omvw.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=udacity-project3-s3-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing12312321
+export URL=http://localhost:8100
