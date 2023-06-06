1e70022b507f370f26e3237f26d8b3428186c1cd
hoantat21 <36986868+hoantat21@users.noreply.github.com>
2023-06-07 00:20:05 +0700

    add deployment file

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a61aee10c43ad49f79308b7ababca95f-1892940608.us-east-1.elb.amazonaws.com:8080/api/v0'
+
 };
 
 /*
